49af8559cad855ffbae62d2094e6242af5641d20
mschild91 <michael.schild@mail.de>
2023-08-22 14:21:26 +0200

    finished the styling for products list

--- src/components/pages/ProductList/ProductList.jsx
+++ src/components/pages/ProductList/ProductList.jsx
@@ -3,13 +3,17 @@ import { apiBaseLink } from "../../../utility/apiBaseLink.js"
 import { useEffect, useState } from "react"
 import ProductItem from "../../shared/ProductItem/ProductItem"
 import Navbar from "../../layout/Navbar/Navbar"
+import { AutoFlex } from "../../shared/AutoFlex/AutoFlex.jsx"
+import { Searchbar } from "../../shared/Searchbar/Searchbar"
+import { FilterMenu } from "../../shared/FilterMenu/FilterMenu"
 
 export const ProductList = () => {
   const [products, setProducts] = useState([])
   const [isLoading, setIsLoading] = useState(true)
+  const [filterMenu, setFilterMenu] = useState(false)
 
   useEffect(() => {
-    fetch(`${apiBaseLink}`)
+    fetch(`${apiBaseLink}?limit=100`)
       .then((response) => {
         if (!response.ok) {
           throw new Error("Fetch failed")
@@ -21,23 +25,35 @@ export const ProductList = () => {
         setIsLoading(false)
       })
       .catch((error) => console.log(error.message))
-  })
+  }, [])
 
   if (isLoading) {
     return <p>Loading...</p>
   }
 
   return (
-    <>
-      <section>
-        {products.map((product) => (
-          <ProductItem
-            key={product.id}
-            product={product}
-          />
-        ))}
-      </section>
-      <Navbar />
-    </>
+    <section className={styles.wrapper}>
+      {filterMenu ? (
+        <FilterMenu onClickP={() => setFilterMenu(false)} />
+      ) : (
+        <>
+          <Searchbar onClickP={() => setFilterMenu((prevState) => true)} />
+          <p className={styles.sort}>
+            Sort by: <span>Lowest Price</span>
+          </p>
+          <section>
+            <AutoFlex>
+              {products.map((product) => (
+                <ProductItem
+                  key={product.id}
+                  product={product}
+                />
+              ))}
+            </AutoFlex>
+          </section>
+          <Navbar />
+        </>
+      )}
+    </section>
   )
 }
